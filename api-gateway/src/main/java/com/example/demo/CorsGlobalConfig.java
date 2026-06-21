646191069571377c23b3d7fe77b3a3c80e2bcb0d
vishalkashid08 <vishalkashid08@gmail.com>
2026-06-21 12:04:03 +0530

    corsGlobleConfig File uploaded

---
+++ api-gateway/src/main/java/com/example/demo/CorsGlobalConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://3.110.167.15:5173")); // frontend URL
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
